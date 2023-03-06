6341ec4f33f01bd328646e2682996b38f21f82f1
River Hansen <riverbh01@gmail.com>
2023-03-06 14:57:48 -0700

    second commit for back end demo

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+let database = ['greeting card', 'wagon', 
+'computer', 'table','chair', 'milk', 
+'sailboat','conditioner', 'rusty nail', 
+'desk']
+
+app.get('/api/inventory', (req, res) => {
+    let item = req.query.item
+
+    if (item) {
+        item = item.toLowerCase()
+        let resposneArr = []
+
+        for(let i = 0; i < database.length; i++) {
+            if (database[i].includes(item)){
+                resposneArr.push(database[i])
+            }
+        }
+        res.status(200).send(resposneArr)
+    }else{
+        res.status(200).send(database)
+    }
+})
+
+app.get('/api/inventory/:id', (req, res) => {
+    let id = req.params.id 
+
+    res.status(200).send(database[id])
+})
+
+app.listen(5050, () => [
+    console.log('app listening on 5050')
+])
